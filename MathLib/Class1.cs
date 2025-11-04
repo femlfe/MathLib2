a51603b920f0853780307fcd8c60365f8e12668f
Novozhilova Ksenia <nowksusha@mail.ru>
2025-11-04 09:12:15 +0300

    inithial repo

---
+++ MathLib/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace MathLib
+{
+    public class Culculation
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Factorial(int n)
+        {
+            if (n == 0) return 1;
+            else return n*Factorial(n-1);
+        }
+
+    }
+}
---
+++ MathLibTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+using MathLib;
+
+namespace MathLibTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void CheckSumResult()
+        {
+            // Arrange
+            Culculation culculation = new Culculation();
+
+            // Act
+            var result = culculation.Sum(2, 3);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void CheckFactorialResult()
+        {
+            // Arrange
+            Culculation culculation = new Culculation();
+
+            // Act
+            var result = culculation.Factorial(4);
+
+            // Assert
+            Assert.Equal(2 ,result);
+        }
+    }
+}
